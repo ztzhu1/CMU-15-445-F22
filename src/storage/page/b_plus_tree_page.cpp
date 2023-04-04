acd6a448584e5b4d743c0a11b98d715568eaa394
ztzhu1 <zhuzitian@zju.edu.cn>
2023-04-04 23:15:37 +0800

    Page utils

--- src/storage/page/b_plus_tree_page.cpp
+++ src/storage/page/b_plus_tree_page.cpp
@@ -10,6 +10,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/page/b_plus_tree_page.h"
+#include <cassert>
 
 namespace bustub {
 
@@ -17,41 +18,62 @@ namespace bustub {
  * Helper methods to get/set page type
  * Page type enum class is defined in b_plus_tree_page.h
  */
-auto BPlusTreePage::IsLeafPage() const -> bool { return false; }
-auto BPlusTreePage::IsRootPage() const -> bool { return false; }
-void BPlusTreePage::SetPageType(IndexPageType page_type) {}
+auto BPlusTreePage::IsLeafPage() const -> bool { return page_type_ == IndexPageType::LEAF_PAGE; }
+auto BPlusTreePage::IsInternalPage() const -> bool { return page_type_ == IndexPageType::INTERNAL_PAGE; }
+auto BPlusTreePage::IsRootPage() const -> bool { return parent_page_id_ == INVALID_PAGE_ID; }
+void BPlusTreePage::SetPageType(IndexPageType page_type) { page_type_ = page_type; }
 
 /*
  * Helper methods to get/set size (number of key/value pairs stored in that
  * page)
  */
-auto BPlusTreePage::GetSize() const -> int { return 0; }
-void BPlusTreePage::SetSize(int size) {}
-void BPlusTreePage::IncreaseSize(int amount) {}
+auto BPlusTreePage::GetSize() const -> int { return size_; }
+void BPlusTreePage::SetSize(int size) { size_ = size; }
+void BPlusTreePage::IncreaseSize(int amount) { size_ += amount; }
+void BPlusTreePage::DecreaseSize(int amount) {
+  assert(size_ >= amount);
+  size_ -= amount;
+}
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
-auto BPlusTreePage::GetMaxSize() const -> int { return 0; }
-void BPlusTreePage::SetMaxSize(int size) {}
+auto BPlusTreePage::GetMaxSize() const -> int { return max_size_; }
+void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
 
 /*
  * Helper method to get min page size
  * Generally, min page size == max page size / 2
  */
-auto BPlusTreePage::GetMinSize() const -> int { return 0; }
+auto BPlusTreePage::GetMinSize() const -> int {
+  if (IsRootPage()) {
+    if (IsLeafPage()) {
+      return 1;
+    }
+    // internal page
+    return 2;
+  }
+  if (IsLeafPage()) {
+    return max_size_ / 2;  // floor(max_size/2)
+  }
+  // internal page
+  return (max_size_ + 2 - 1) / 2;  // ceil(max_size/2)
+}
 
+auto BPlusTreePage::SafeToInsert() const -> bool { return size_ < max_size_ - 1; }
+
+auto BPlusTreePage::SafeToRemove() const -> bool { return size_ > GetMinSize(); }
 /*
  * Helper methods to get/set parent page id
  */
-auto BPlusTreePage::GetParentPageId() const -> page_id_t { return 0; }
-void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) {}
+auto BPlusTreePage::GetParentPageId() const -> page_id_t { return parent_page_id_; }
+void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) { parent_page_id_ = parent_page_id; }
 
 /*
  * Helper methods to get/set self page id
  */
-auto BPlusTreePage::GetPageId() const -> page_id_t { return 0; }
-void BPlusTreePage::SetPageId(page_id_t page_id) {}
+auto BPlusTreePage::GetPageId() const -> page_id_t { return page_id_; }
+void BPlusTreePage::SetPageId(page_id_t page_id) { page_id_ = page_id; }
 
 /*
  * Helper methods to set lsn
